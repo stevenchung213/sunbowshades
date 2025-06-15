1929318bd1e71fdca764c0faf5abafb4da9d5604
Steven Chung <stevenchung.213@gmail.com>
2025-06-15 02:16:54 -0700

    add responsive styling for privacy/terms document pages

--- src/privacy/Privacy.jsx
+++ src/privacy/Privacy.jsx
@@ -4,81 +4,81 @@ import './styles.css';
 const PrivacyPolicy = () => {
   return (
     <div className='document_container'>
-      <h1>Privacy Policy</h1>
-      <p>
+      <h1 className='document_header'>Privacy Policy</h1>
+      <p className='document_paragraph'>
         We value your privacy and are committed to protecting your personal information.
         <br />
         This Privacy Policy outlines how we collect, use, and safeguard your data when you interact with our website or otherwise engage with us.
       </p>
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Data Collection
       </h3>
-      <p>
+      <p className='document_paragraph'>
         When you visit our website and/or use our services, we may collect the following types of information:
         <br />
-        <i>Personal Information:</i>
+        <i className='document_italics'>Personal Information:</i>
         <br />
         We may collect your full name, email address, phone number, and home address if
         you decide to utilize our services.
         <br />
-        <i>Customer Support:</i>
+        <i className='document_italics'>Customer Support:</i>
         <br />
         When you contact us for customer support, we may collect additional information to resolve
         your inquiry.
         <br />
-        <i>Purpose of Collection:</i>
+        <i className='document_italics'>Purpose of Collection:</i>
         <br />
         The collected information is used to process orders, communicate with you about your purchases, and provide you
-        with better services. <b>We do not sell or rent your personal information to third parties.</b>
+        with better services. <b className='document_bold'>We do not sell or rent your personal information to third parties.</b>
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Cookies and Tracking
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We may use cookies to improve the user experience on our website. A cookie is a small file of data that is placed on
         your device by our website. If you continue browsing after having been directed to this Privacy Policy, you agree
         to the use of cookies on our site.
         <br />
-        <i>Third-party cookies:</i>
+        <i className='document_italics'>Third-party cookies:</i>
         <br />
         We may also use third-party analytics tools (e.g., Google Analytics) to track website
         usage and improve our services. These tools may collect data about your online behavior on our site.
         <br />
-        <i>Your Choices:</i>
+        <i className='document_italics'>Your Choices:</i>
         <br />
         You can disable cookies in your browser settings, but this may affect the functionality of our website.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Third-Party Services
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We currently use third-party services to analyze website traffic. These third-party providers
         are bound by their own privacy policies, which may include the collection of additional data.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Data Security
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We take reasonable steps to protect your personal information from unauthorized access, use, or disclosure. This
         includes but is not limited to:
       </p>
-      <p>
-        <i>- Encrypting your data when it is transmitted over the internet.</i>
+      <p className='document_paragraph'>
+        <i className='document_italics'>- Encrypting your data when it is transmitted over the internet.</i>
       </p>
-      <p>
-        <i>- Implementing security measures to prevent unauthorized access.</i>
+      <p className='document_paragraph'>
+        <i className='document_italics'>- Implementing security measures to prevent unauthorized access.</i>
       </p>
-      <p>
-        <i>- Regularly updating and maintaining our systems to protect against vulnerabilities.</i>
+      <p className='document_paragraph'>
+        <i className='document_italics'>- Regularly updating and maintaining our systems to protect against vulnerabilities.</i>
       </p>
-      <p>
+      <p className='document_paragraph'>
         <br />
         We comply with any and all applicable privacy laws (e.g., GDPR in the EU) if you are a resident of those regions. We will
         ensure your rights under these laws are respected, including providing you with access to, correction, or deletion
@@ -86,67 +86,67 @@ const PrivacyPolicy = () => {
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         User Rights
       </h3>
-      <p>You have the right to:</p>
-      <p>
+      <p className='document_paragraph'>You have the right to:</p>
+      <p className='document_paragraph'>
 
-        <i>- Request access to your personal information.</i>
+        <i className='document_italics'>- Request access to your personal information.</i>
         <br />
-        <i>- Correct or update your personal information.</i>
+        <i className='document_italics'>- Correct or update your personal information.</i>
         <br />
-        <i>- Delete and remove your personal information from our records, subject to legal and contractual
+        <i className='document_italics'>- Delete and remove your personal information from our records, subject to legal and contractual
           obligations.</i>
         <br />
-        <i>- Opt out of marketing communications.</i>
+        <i className='document_italics'>- Opt out of marketing communications.</i>
       </p>
-      <p>
+      <p className='document_paragraph'>
         Please contact us to exercise these rights.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Data Retention
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We will retain your personal information for as long as necessary to process transactions, provide customer
         support, or comply with legal and regulatory obligations. Once no longer needed, we will securely delete the data
         in accordance with our data retention policy.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Breach Notification
       </h3>
-      <p>
+      <p className='document_paragraph'>
         In the unlikely event of a data breach that compromises your personal information, we will notify affected
         individuals via email or another reasonable method as required by law. We will work to restore confidence in our
         systems and take steps to prevent future breaches.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Children's Privacy
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We do not knowingly collect personal information from children under the age of 18 without parental consent. If
         you are a minor, please have your parent or guardian contact us to review any data collection practices specific
         to children.
       </p>
       <br />
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Changes to This Policy
       </h3>
-      <p>
+      <p className='document_paragraph'>
         We may update this Privacy Policy from time to time. We will post any changes on this page and notify you via
         email if the changes significantly impact your rights or obligations under the policy.
       </p>
-      <p>
+      <p className='document_paragraph'>
         <br />
         <br />
-        <i>Thank you for trusting SunbowShades with your personal information. We are committed to
+        <i className='document_italics'>Thank you for trusting SunbowShades with your personal information. We are committed to
           protecting it and ensuring transparency in how we use it.</i>
       </p>
       <br />
--- src/terms/Terms.jsx
+++ src/terms/Terms.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import './styles.css';
 
 const Terms = () => {
   return (
     <div className='document_container'>
-      <h1>Terms of Use Agreement for SunbowShades</h1>
-      <p>
+      <h1 className='document_header'>Terms of Use Agreement for SunbowShades</h1>
+      <p className='document_paragraph'>
         Welcome to our website at www.sunbowshades.com.
         By accessing or using this site, you agree to be bound by the following Terms of Use Agreement. If you do not agree with any part of these terms, we encourage you to refrain from using our site.
       </p>
       <br />
-      <h3>
+      <h3 className='document_subheader'>
         Governing Law and Jurisdiction
       </h3>
-      <p>
+      <p className='document_paragraph'>
         This Agreement shall be governed by and construed in accordance with the laws of CA, OR, WA, NV and TX, without regard to its conflict of law principles. Any legal action or proceeding arising out of or related to this Agreement must be brought exclusively in the courts of CA, OR, WA, NV and TX. You agree to submit to the exclusive jurisdiction of such courts for any claims, disputes, or proceedings relating to these terms.
       </p>
-      <h3>Purpose and Scope</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Purpose and Scope</h3>
+      <p className='document_paragraph'>
         This Agreement outlines the conditions under which you may access and use our site. It also governs your submission of personal information through the Site, as well as your use of copyrighted images accessible via the site.
       </p>
-      <h3>User Responsibilities</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>User Responsibilities</h3>
+      <p className='document_paragraph'>
         You agree to:
         <br />
         - Access the site for lawful purposes only.
@@ -33,44 +34,52 @@ const Terms = () => {
         <br />
         - Refrain from engaging in illegal activities, including unauthorized use of intellectual property rights.
       </p>
-      <h3>Personal Information Use</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Personal Information Use</h3>
+      <p className='document_paragraph'>
         We collect your personal information name, email, phone number, address to provide services, improve user
         experience, and communicate with you. We do not sell your data to third parties unless necessary for operational
         purposes or required by law. You may revoke consent at any time by updating your account settings.
       </p>
-      <h3>Intellectual Property Rights</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Intellectual Property Rights</h3>
+      <p className='document_paragraph'>
         All content, including images, on this site is the property of SunbowShades and is protected
         under copyright laws. Users are prohibited from downloading, copying, or modifying such images without prior written consent.
       </p>
-      <h3>Dispute Resolution</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Dispute Resolution</h3>
+      <p className='document_paragraph'>
         Any disputes arising out of or related to this Agreement shall be resolved by arbitration in CA, OR, WA, NV and TX, as specified in the applicable rules of the American Arbitration Association. The AAA Rules for Consumer Disputes apply, and any arbitration proceedings must be conducted in English.
       </p>
-      <h3>Severability</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Severability</h3>
+      <p className='document_paragraph'>
         If any part of this Agreement is found to be invalid or unenforceable, only that part shall be excluded, and the remainder shall remain in effect.
       </p>
-      <h3>Amendments</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Amendments</h3>
+      <p className='document_paragraph'>
         We reserve the right to amend these terms at any time without prior notice. Any amendments will become effective immediately upon posting on the site. Continued use of the site after changes constitutes acceptance of the new
         terms.
       </p>
-      <h3>Contact Information</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Contact Information</h3>
+      <p className='document_paragraph'>
         For support or feedback, please contact us at sunbowshades@gmail.com or 323-577-5705. We value your input and appreciate your assistance in maintaining a positive user experience.
       </p>
-      <h3>Agreement Binding</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Agreement Binding</h3>
+      <p className='document_paragraph'>
         This Agreement is a legally binding contract between you and SunbowShades. Breach of these
         terms may result in legal action and/or liability for damages.
         <br />
         Your use of this site signifies your agreement to abide by the above terms and conditions. We encourage you to
         review these terms regularly, as they are subject to change.
       </p>
-      <h3>Version History</h3>
-      <p>
+      <br />
+      <h3 className='document_subheader'>Version History</h3>
+      <p className='document_paragraph'>
         We maintain records of all versions of this Agreement on our servers. Any changes will be noted, ensuring a clear
         record for users.
         <br />
@@ -79,8 +88,8 @@ const Terms = () => {
       </p>
       <br />
       <br />
-      <p>
-        <i className='document_end'>
+      <p className='document_paragraph'>
+        <i className='document_italics'>
           Thank you for choosing SunbowShades. Enjoy your experience with our services and site features!
         </i>
       </p>
